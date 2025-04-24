e434de8262d84a301b83c5621c7ff8ff32c8e9d9
bantee-sharma <banteesharma9@gmail.com>
2025-04-24 21:17:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,29 +4,39 @@ from langchain_core.prompts import PromptTemplate
 from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.chains import retrieval_qa
+
 
 video_id = 'Gfr50f6ZBvo'
 
 try:
-    transcript_list = YouTubeTranscriptApi.get_transcript(video_id=video_id,languages=["en"])
-
-    transcript = " ".join(i["text"] for i in transcript_list)
+    transcript_text = YouTubeTranscriptApi.get_transcript(video_id=video_id,languages=["en"])
+    text = " ".join([i["text"] for i in transcript_text])
 except TranscriptsDisabled:
-    print("No caption availble for this video")
 
 text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=200
+    chunk_size=1000,chunk_overlap=200
 )
 
-chunk = text_splitter.create_documents([transcript])
+chunk = text_splitter.create_documents([text])
 
+embeddings = HuggingFaceEmbeddings()
 
-embeddings =HuggingFaceEmbeddings()
+vector_store = FAISS.from_documents(chunk,embedding=embeddings)
 
-db = FAISS.from_documents(chunk,embedding=embeddings)
+retriever = vector_store.as_retriever(search_type="similarity", kwargs={"k":1})
 
-retriever = db.as_retriever(search_type="similarity", search_kwargs={"k":4})
+res = retriever.invoke("What id deepmind?")
 
-res = retriever.invoke("what is deepmind")
+prompt = PromptTemplate(
+    template='''You are a helpfull assistant. Answer only from provided transcript context. if context is insufficient, just say you do not know.
 
-print(res)
+    {context}
+    Question = {question}''',
+    input_variables=["context","question"]
+)
+
+question = "Is there any topic disscused about aliens in this video"
+
+ret_docs = retriever.invoke(question)
+print(ret_docs)
