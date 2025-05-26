789834428fa73c0097da9757149717b957c1f1e9
bantee-sharma <banteesharma9@gmail.com>
2025-05-26 13:45:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,57 @@
-from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
-from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
-from langchain_core.prompts import PromptTemplate
-from langchain_community.vectorstores import FAISS
+from youtube_transcript_api import YouTubeTranscriptApi,TranscriptsDisabled
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.chains import retrieval_qa
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
 
-
-video_id = 'Gfr50f6ZBvo'
-
-try:
-    transcript_text = YouTubeTranscriptApi.get_transcript(video_id=video_id,languages=["en"])
-    text = " ".join([i["text"] for i in transcript_text])
-except TranscriptsDisabled:
-    print("No captions available for this video.")
-
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,chunk_overlap=200
-)
-
-chunk = text_splitter.create_documents([text])
-
-embeddings = HuggingFaceEmbeddings()
-
-vector_store = FAISS.from_documents(chunk,embedding=embeddings)
-
-retriever = vector_store.as_retriever(search_type="similarity", kwargs={"k":4})
-
-res = retriever.invoke("What id deepmind?")
-
-prompt = PromptTemplate(
-    template='''You are a helpfull assistant. Answer only from provided transcript context. if context is insufficient, just say you do not know.
-
-    {context}
-    Question = {question}''',
-    input_variables=["context","question"]
-)
-
-question = "Is there any topic disscused about aliens in this video, if yes then what was discussed"
-
-retriever_docs = retriever.invoke(question)
-
-cont = " ".join([i.page_content for i in retriever_docs])
+load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
-final_prompt = prompt.invoke({"context":cont, "question": question})
+video_id = 'E3oG313_kps'
 
-answer = llm.invoke(final_prompt)
-print(answer.content)
+try:
+    transcript_text = YouTubeTranscriptApi.get_transcript(video_id=video_id,languages=['hi'])
+    text = " ".join([i["text"] for i in transcript_text])
+    
 
+except TranscriptsDisabled:
+    print("No captions available for this video.")
+
+
+text_split = RecursiveCharacterTextSplitter(
+    chunk_size = 1000, chunk_overlap = 100
+)
+
+chunk = text_split.create_documents([text])
+
+embedd = HuggingFaceEmbeddings()
+
+vector_store = FAISS.from_documents(chunk,embedd)
+
+retriever = vector_store.as_retriever(search_type="similarity", kwargs={'k':3})
+
+prompt = PromptTemplate(
+    template= '''You are a helpful AI assistant.
+    Answer the question based on the following context.
+    If context is insufficient, just say, "I don't know."
+    if anyone ask the quesion in english, then answer in Englsih.
+    {context}
+    Question :
+    {question}''',
+    input_variables=["context","question"]
+)
+
+question = "Summary of this video"
+
+retriever_docs = retriever.invoke(question)
+
+context = " ".join([i.page_content for i in retriever_docs])
+
+final_prompt = prompt.invoke({"context":context, "question": question})
+
+res = llm.invoke(final_prompt)
+
+print(res.content)
