863d553c2ececb329ab7fb02b137acfc193b1261
bantee-sharma <banteesharma9@gmail.com>
2025-06-03 21:26:00 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,4 +1,8 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+from youtube_transcript_api import  YouTubeTranscriptApi, TranscriptsDisabled
 
 load_dotenv()
+
+video_id = "E3oG313_kps"
+
