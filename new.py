5916b9655942c46a45d6e6041df36324c08cacbf
bantee-sharma <banteesharma9@gmail.com>
2025-06-03 21:36:27 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,10 +1,14 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-from youtube_transcript_api import  YouTubeTranscriptApi, TranscriptsDisabled
+from youtube_transcript_api import  YouTubeTranscriptApi, TranscriptsDisabled,NoTranscriptFound
 
 load_dotenv()
 
 video_id = "E3oG313_kps"
 
-transcript_text = YouTubeTranscriptApi.get_transcript(video_id=video_id,languages=["hi","en"])
-print(transcript_text)
+try:
+    transcript_text = YouTubeTranscriptApi.get_transcript(video_id=video_id,languages=["hi"])
+    print(transcript_text)
+
+except NoTranscriptFound:
+    print("No Captions found for this video.")
