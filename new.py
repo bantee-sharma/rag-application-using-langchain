fc3fad52c567f321e05731a65001545925726b5e
bantee-sharma <banteesharma9@gmail.com>
2025-06-03 21:48:38 +0530

    Update new.py

--- new.py
+++ new.py
@@ -2,6 +2,8 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from youtube_transcript_api import  YouTubeTranscriptApi, TranscriptsDisabled,NoTranscriptFound
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 
 load_dotenv()
 
@@ -15,6 +17,13 @@ try:
 except NoTranscriptFound:
     print("No Captions found for this video.")
 
-text_spiltter = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=100)
+text_spiltter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=100)
 chunks = text_spiltter.create_documents([text])
-print(len(chunks))
+
+embedd = HuggingFaceEmbeddings()
+vector_store = FAISS.from_documents(chunks,embedd)
+
+retriever = vector_store.as_retriever(search_type = "similarity",kwargs={"k":5})
+
+query = "What is this about?"
+print(retriever.invoke(query))
