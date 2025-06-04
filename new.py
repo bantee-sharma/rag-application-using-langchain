3978f41ebf06759a92069ed1495f30acfaf57130
bantee-sharma <banteesharma9@gmail.com>
2025-06-04 22:18:56 +0530

    Update new.py

--- new.py
+++ new.py
@@ -8,6 +8,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from langchain.agents import tool,AgentExecutor,create_react_agent
 from langchain import hub
+import requests
 
 load_dotenv()
 
@@ -61,7 +62,12 @@ def doc_qa_tool(question:str)->str:
 @tool
 def weather(city:str)->str:
     "fetch the current weather"
-    return f"the weather of {city} is 25"
+    url = "https://api.weatherstack.com/current?access_key=2eab0e8b57aa4c3082a9f22e95baa467"
+
+    city_name = city
+    res = requests.get(url=url,params=city_name)
+    return res.json()
+
 
 prompt = hub.pull("hwchase17/react")
 
