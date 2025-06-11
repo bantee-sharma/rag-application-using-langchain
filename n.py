fa76054b8635bbd1b7b18171da04fedf82bfc65c
bantee-sharma <banteesharma9@gmail.com>
2025-06-11 21:30:12 +0530

    Update n.py

--- n.py
+++ n.py
@@ -1,6 +1,9 @@
 from langchain_community.document_loaders import PyMuPDFLoader,TextLoader, UnstructuredWordDocumentLoader
 from pathlib import Path
-
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import PromptTemplate
 
 def my_docs(folder_path):
     all_docs = []
@@ -20,5 +23,22 @@ def my_docs(folder_path):
     return all_docs
 
 doc = my_docs("docs")
-print(doc)
 
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+chunks = text_splitter.split_documents(doc)
+
+embedd = HuggingFaceEmbeddings()
+db = FAISS.from_documents(chunks,embedd)
+
+retriever = db.as_retriever(search_type="similarity",kwargs={"k":5})
+
+prompt = PromptTemplate(
+    template='''You are a helpfull Assistant.
+    Answer the question from the following context : {context}.
+    If the answer is not present in the context, respond with: "The answer is not available in the provided context.Question: {question}''',
+    input_variables=["context","question"]
+)
+
+query = "What is sql?"
+context = retriever.invoke(query)
+print(context)
