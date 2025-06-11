09e229c6226a1de564a3c826515739986691bc7a
bantee-sharma <banteesharma9@gmail.com>
2025-06-11 21:56:13 +0530

    Update n.py

--- n.py
+++ n.py
@@ -6,6 +6,10 @@ from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain import hub
+import requests
 
 load_dotenv()
 
@@ -30,25 +34,55 @@ def my_docs(folder_path):
 
 doc = my_docs("docs")
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-chunks = text_splitter.split_documents(doc)
+@tool
+def doc_qa_tool(question:str)->str:
+    "Yor are a helpfull AI assistant. Answer the question from the following context."
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    chunks = text_splitter.split_documents(doc)
 
-embedd = HuggingFaceEmbeddings()
-db = FAISS.from_documents(chunks,embedd)
+    embedd = HuggingFaceEmbeddings()
+    db = FAISS.from_documents(chunks,embedd)
 
-retriever = db.as_retriever(search_type="similarity",kwargs={"k":5})
+    retriever = db.as_retriever(search_type="similarity",kwargs={"k":5})
 
-prompt = PromptTemplate(
-    template='''You are a helpfull Assistant.
-    Answer the question from the following context : {context}.
-    If the answer is not present in the context, respond with: "The answer is not available in the provided context.Question: {question}''',
-    input_variables=["context","question"]
+    qa_prompt = PromptTemplate(
+        template='''You are a helpfull Assistant.
+        Answer the question from the following context : {context}.
+        If the answer is not present in the context, respond with: "The answer is not available in the provided context.Question: {question}''',
+        input_variables=["context","question"]
+    )
+    context = retriever.invoke(question)
+    final_prompt = qa_prompt.invoke({"question":question,"context":context})
+    res = llm.invoke(final_prompt)
+    return res.content
+
+@tool
+def weather(city:str)-> str:
+    "Fetch the Current weather of the city"
+    url = "https://api.weatherstack.com/current?access_key=2eab0e8b57aa4c3082a9f22e95baa467"
+    city_srting = {"input":city}
+    temp = requests.get(url=url,params=city_srting)
+    return temp.json()
+
+prompt = hub.pull("rwchase17/react")
+
+agent = create_react_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=[doc_qa_tool,weather]
 )
 
-query = "The Evolution of Football Tactics"
-context = retriever.invoke(query)
-
-final_prompt = prompt.invoke({"question":query,"context":context})
-
-res = llm.invoke(final_prompt)
-print(res)
+age_exe = AgentExecutor(
+    agent=agent,
+    tools=[doc_qa_tool,weather],
+    verbose=True
+)
+print("Hii, I am you personal assistant ask me anything!. For quiting conversation type exit or quit")
+while True:
+    question = input("Ask me: ").strip()
+    if question.lower() in ["quit","exit"]:
+        print("Exiting...Bye....")
+        break
+    else:
+        response =age_exe.invoke(question)
+        print("AI: ",response)
