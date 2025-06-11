375e11e48ef1fc6d4aa3aba7e9aed847175081cc
bantee-sharma <banteesharma9@gmail.com>
2025-06-11 21:09:20 +0530

    Update n.py

---
+++ n.py
@@ -0,0 +1,23 @@
+from langchain.document_loaders import PyMuPDFLoader,TextLoader, UnstructuredWordDocumentLoader
+from pathlib import Path
+
+
+def my_docs(folder_path):
+    all_docs = []
+    for file in Path(folder_path).glob("*"):
+        if file.suffix == ".pdf":
+            loader = PyMuPDFLoader(str(file))
+        elif file.suffix == ".txt":
+            loader = TextLoader(str(file),encoding="utf-8")
+        elif file.suffix == ".docx":
+            loader = UnstructuredWordDocumentLoader(str(file))
+        else:
+            return "No filefound"
+        
+        document = loader.lazy_load()
+        all_docs.extend(document)
+    return all_docs
+
+doc = my_docs("docs")
+print(doc)
+
