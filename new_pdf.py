ff2497d0f3f6c991afc31b8128ba55621e4635d3
bantee-sharma <banteesharma9@gmail.com>
2025-06-21 20:52:24 +0530

    Update new_pdf.py

--- new_pdf.py
+++ new_pdf.py
@@ -1,2 +1,9 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.document_loaders import PyMuPDFLoader
+
+
+path = "docs\PA - Consolidated lecture notes.pdf"
+loader = PyMuPDFLoader(path)
+
+document = loader.load()
+print(document)
