620bcbf5c8bbcab454e783bc73c29a400906495b
bantee-sharma <banteesharma9@gmail.com>
2025-06-21 20:57:05 +0530

    Update new_pdf.py

--- new_pdf.py
+++ new_pdf.py
@@ -1,8 +1,13 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.document_loaders import PyMuPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 path = "docs\PA - Consolidated lecture notes.pdf"
 loader = PyMuPDFLoader(path)
-
 document = loader.load()
-print(document)
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=100)
+chunks = text_splitter.split_documents(document)
+print(len(chunks))
+
+
