e2a4fb516841210506b1a2abd1375d7822c85f23
bantee-sharma <banteesharma9@gmail.com>
2025-06-22 21:01:58 +0530

    Update n.py

--- n.py
+++ n.py
@@ -1,10 +1,21 @@
 from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
 
+load_dotenv()
 
-
-
-
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
 loader = PyMuPDFLoader("docs\PA - Consolidated lecture notes.pdf")
 doc = loader.load()
-print(doc)
+
+prompt = PromptTemplate(
+    template="Summarize the following text: {text}",
+    input_variables=["text"]
+)
+
+chain = prompt | llm
+
+res = chain.invoke({"text":doc})
+print(res)
