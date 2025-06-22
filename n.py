8d3b4313780ee1725c27601ce34a60707cf84bda
bantee-sharma <banteesharma9@gmail.com>
2025-06-22 21:08:23 +0530

    Update n.py

--- n.py
+++ n.py
@@ -2,13 +2,17 @@ from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
 loader = PyMuPDFLoader("docs\PA - Consolidated lecture notes.pdf")
-doc = loader.load()
+docs = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+chunks = splitter.split_documents(docs)
 
 prompt = PromptTemplate(
     template="Summarize the following text: {text}",
@@ -17,5 +21,5 @@ prompt = PromptTemplate(
 
 chain = prompt | llm
 
-res = chain.invoke({"text":doc})
-print(res)
+res = chain.invoke({"text":text})
+print(res.content)
