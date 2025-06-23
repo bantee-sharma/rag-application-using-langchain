4bd1564ab8c8b38123c3403fc9ab2c7d50c02683
bantee-sharma <banteesharma9@gmail.com>
2025-06-23 21:48:37 +0530

    Update n.py

--- n.py
+++ n.py
@@ -1,10 +1,22 @@
 from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
 
+load_dotenv()
 
-
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
 loader = PyMuPDFLoader("docs\PA - Consolidated lecture notes.pdf")
 doc = loader.load()
 
 text = "\n".join([i.page_content for i in doc])
-print(len(text))
+
+prompt = PromptTemplate(
+    template="You are Ai summarizer. Summarize the following text: \n {text}",
+    input_variables=["text"]
+)
+
+chain = prompt | llm 
+response = chain.invoke(text)
+print(response.content)
