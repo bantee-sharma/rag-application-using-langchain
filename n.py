18cbe7e9fb9c87dc064a5d3b7ebe7c159b6408e7
bantee-sharma <banteesharma9@gmail.com>
2025-06-23 21:44:48 +0530

    Update n.py

--- n.py
+++ n.py
@@ -1,3 +1,10 @@
 from langchain_community.document_loaders import PyMuPDFLoader
 
 
+
+
+loader = PyMuPDFLoader("docs\PA - Consolidated lecture notes.pdf")
+doc = loader.load()
+
+text = "\n".join([i.page_content for i in doc])
+print(len(text))
