9cd67cc21634bbb36230b6f6548eae42c1b9c181
bantee-sharma <banteesharma9@gmail.com>
2025-06-23 21:42:44 +0530

    Update n.py

--- n.py
+++ n.py
@@ -1,9 +1,1 @@
-import fitz
-
-doc = fitz.open("docs\dl-curriculum.pdf")
-
-
-for page_no in range(len(doc)):
-    page = doc(page_no)
-    text = page.get_text()
-    print(text)
+from langchain_community
