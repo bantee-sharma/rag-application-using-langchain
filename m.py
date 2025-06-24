8e9cc4f5bbfca349f56535344712c13e4990120a
bantee-sharma <banteesharma9@gmail.com>
2025-06-24 21:35:17 +0530

    Create m.py

---
+++ m.py
@@ -0,0 +1,14 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+import streamlit as st
+import tempfile
+
+st.title("AI-Powered PDF Summarizer")
+st.header("Instantly Summarize Lengthy PDFs with the Power of Generative AI")
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
