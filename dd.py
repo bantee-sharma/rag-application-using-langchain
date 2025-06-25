aa0f0d63dc2678f33df813ea6e24c3356297d279
bantee-sharma <banteesharma9@gmail.com>
2025-06-25 21:03:16 +0530

    Update dd.py

--- dd.py
+++ dd.py
@@ -16,32 +16,32 @@ if file is not None:
         tmp_file.write(file.read())
         file_path = tmp_file.name
 
-        loader = PyPDFLoader(file_path)
-        docs = loader.load()
-        text = "\n".join([i.page_content for i in docs])
+    loader = PyPDFLoader(file_path)
+    docs = loader.load()
+    text = "\n".join([i.page_content for i in docs])
 
-        prompt1 = PromptTemplate(
-            template="Summarize the following text:\n {text}",
+    prompt1 = PromptTemplate(
+        template="Summarize the following text:\n {text}",
             input_variables=["text"]
         )
 
-        prompt2 = PromptTemplate(
+    prompt2 = PromptTemplate(
             template="Answer the question:\n{question} from the following text:\n{text}",
             input_variables=["question","text"]
         )
 
-        chain1 = prompt1 | llm
-        chain2 = prompt2 | llm
+    chain1 = prompt1 | llm
+    chain2 = prompt2 | llm
 
-        if st.button("Summarize"):
-            res = chain1.invoke({"text":text})        
-            st.write(res.content)
+    if st.button("Summarize"):
+        res = chain1.invoke({"text":text})        
+        st.write(res.content)
 
 
-        question = st.text_input("Enter your question")
-        if st.button("Submit"):
-            res = chain2.invoke({"question":question ,"text":text})        
-            st.write(res.content)
+    question = st.text_input("Enter your question")
+    if st.button("Submit"):
+        res = chain2.invoke({"question":question ,"text":text})        
+        st.write(res.content)
         
 
 st.markdown("---")
