5af280cfe6722d6d0f61f623906291856bf39fd9
bantee-sharma <banteesharma9@gmail.com>
2025-06-29 22:45:02 +0530

    Update t.py

--- t.py
+++ t.py
@@ -1,8 +1,8 @@
 import fitz
 
 docs = fitz.open("docs/dl-curriculum.pdf")
-d = docs[1].get_text()
-print(d)
-# for page_no in docs:
-#     page = 
-#     print(page)
+
+for page_no in range(len(docs)):
+    page = docs.load_page(page_no)
+    text = page.get_text()
+    print(text)
