3f9918a0d442a60c081db83ceee82f549de00184
bantee-sharma <banteesharma9@gmail.com>
2025-06-29 22:49:40 +0530

    Update t.py

--- t.py
+++ t.py
@@ -1,13 +1,9 @@
-# import fitz
+import fitz
 
-# docs = fitz.open("docs/dl-curriculum.pdf")
+docs = fitz.open("docs/dl-curriculum.pdf")
 
-# for page_no in range(len(docs)):
-#     page = docs.load_page(page_no)
-#     text = page.get_text()
-#     print(f"Page No.{page_no}\n{"---"*30}\n{text}")
+for page_no in range(len(docs)):
+    page = docs.load_page(page_no)
+    text = page.get_text()
+    print(f"Page No.{page_no}\n{"---"*30}\n{text}")
 
-with open("docs/dl-curriculum.pdf","rb") as f:
-    file_name = f.read()
-
-print(file_name)
