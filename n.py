e316ddc33c4381cbe14a3a841446a7cffa2570b0
bantee-sharma <banteesharma9@gmail.com>
2025-07-01 21:27:39 +0530

    Update n.py

--- n.py
+++ n.py
@@ -2,15 +2,24 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import PromptTemplate
+import streamlit as st
+
+
+st.title("AI summarizer")
+st.header("Convert your long text into small summaries")
 
 
 load_dotenv()
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
-loader = PyPDFLoader("docs/dl-curriculum.pdf")
-doc = loader.load()
+file_upload = st.file_uploader("Upload your file",type=[".pdf"])
 
-text = "\n".join([i.page_content for i in doc])
+if file_upload is not None:
+
+    loader = PyPDFLoader("docs/dl-curriculum.pdf")
+    doc = loader.load()
+
+    text = "\n".join([i.page_content for i in doc])
 
 prompt1 = PromptTemplate(
     template="You are an helpfull AI assistant. Summarize the following text:\n {text}",
@@ -18,5 +27,9 @@ prompt1 = PromptTemplate(
 )
 
 chain = prompt1 | llm
-res = chain.invoke({"text":text})
-print(res.content)
+
+
+if st.button("Summarize"):
+
+    res = chain.invoke({"text":text})
+    st.write(res.content)
