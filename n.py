3c45e3f9d1a3b33ff27b64fa173492d7ce8fa307
bantee-sharma <banteesharma9@gmail.com>
2025-07-01 21:23:53 +0530

    Update n.py

--- n.py
+++ n.py
@@ -1,7 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
-
+from langchain_core.prompts import PromptTemplate
 
 
 load_dotenv()
@@ -10,5 +10,13 @@ llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 loader = PyPDFLoader("docs/dl-curriculum.pdf")
 doc = loader.load()
 
-text = " \n ".join([i.page_content for i in doc])
-print(text)
+text = "\n".join([i.page_content for i in doc])
+
+prompt1 = PromptTemplate(
+    template="You are an helpfull AI assistant. Summarize the following text:\n {text}",
+    input_variables=["text"]
+)
+
+chain = prompt1 | llm
+res = chain.invoke({"text":text})
+print(res.content)
