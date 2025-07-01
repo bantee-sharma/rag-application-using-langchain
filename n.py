adec2f95dabb0f744b0faab01c6886ff420ce3e3
bantee-sharma <banteesharma9@gmail.com>
2025-07-01 21:19:32 +0530

    Update n.py

--- n.py
+++ n.py
@@ -5,8 +5,8 @@ from langchain_community.document_loaders import PyPDFLoader
 
 
 load_dotenv()
-llm = ChatGoogleGenerativeAI()
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 
 loader = PyPDFLoader("docs/dl-curriculum.pdf")
 doc = loader.load()
-print(doc)
+print(len(doc))
