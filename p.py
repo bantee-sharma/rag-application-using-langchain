9f1bdf92a26db929ad07e3041fc6278ff351e64a
bantee-sharma <banteesharma9@gmail.com>
2025-07-04 22:01:09 +0530

    Create p.py

---
+++ p.py
@@ -0,0 +1,15 @@
+
+
+
+
+def func(city:str)->bool:
+    """
+    This function checks if the given city is 'New York'.
+    
+    Args:
+    city (str): The name of the city to check.
+    
+    Returns:
+    bool: True if the city is 'New York', False otherwise.
+    """
+    return city == 'New York'
