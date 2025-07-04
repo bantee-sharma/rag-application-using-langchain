903e7b691df13e256d6385f352faac8e3d2f7eb3
bantee-sharma <banteesharma9@gmail.com>
2025-07-04 22:13:58 +0530

    Update p.py

--- p.py
+++ p.py
@@ -1,18 +1,9 @@
 
 
+d = [
+   { {"fruits"}: ["apple", "banana", "cherry"],
+    {"price"}:[100, 200, 300]}
+]
 
-
-
-n = [1,2,3,4,1,2,5,6,3,8,3,6]
-
-def func(n):
-    seen = []
-    s = set()
-    for i in n:
-        if i not in seen:
-            seen.append(i)
-        else:    
-            s.add(i)
-    return list(s)
-
-print(func(n))
+for i in d:
+    print(i)
