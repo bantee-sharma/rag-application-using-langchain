acb997003de455dfabfb0d3ac10a4800b68ff65d
bantee-sharma <banteesharma9@gmail.com>
2025-07-04 22:03:53 +0530

    Update p.py

--- p.py
+++ p.py
@@ -1,18 +1,14 @@
 
 
 
+s = ["words","python","java","c++","javascript","ruby","php","swift","kotlin","typescript"]
 
-def func(city:str)->bool:
-    """
-    This function checks if the given city is 'New York'.
-    
-    Args:
-    city (str): The name of the city to check.
-    
-    Returns:
-    bool: True if the city is 'New York', False otherwise.
-    """
-    return city == 'New York'
 
-print(func('New York'))  # Expected output: True
-print(func('Los Angeles'))  # Expected output: False
+def func(s):
+    new = []
+    for i in s:
+        if len(i) == len(set(i)):
+            new.append(i)
+    return new
+
+print(func(s))
