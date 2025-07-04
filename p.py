71f8340604f45fa000b9cf52ee72f113c59f738f
bantee-sharma <banteesharma9@gmail.com>
2025-07-04 22:09:35 +0530

    Update p.py

--- p.py
+++ p.py
@@ -6,10 +6,12 @@
 n = [1,2,3,4,1,2,5,6,3,8,3,6]
 
 def func(n):
+    seen = []
     s = set()
     for i in n:
-        if i in s:
-            s
-    return list(s)
-
+        if i not in s:
+            seen.append(i)
+        else:    
+            s.add(i)
+    return s
 print(func(n))
