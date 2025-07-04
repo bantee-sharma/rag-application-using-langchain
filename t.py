36c6c2a6ccaaa8c9ad4053206228615d92a0d6c2
bantee-sharma <banteesharma9@gmail.com>
2025-07-04 21:59:45 +0530

    Delete t.py

--- t.py
+++
@@ -1,9 +0,0 @@
-import fitz
-
-docs = fitz.open("docs/dl-curriculum.pdf")
-
-for page_no in range(len(docs)):
-    page = docs.load_page(page_no)
-    text = page.get_text()
-    print(f"Page No.{page_no}\n{"---"*30}\n{text}")
-
