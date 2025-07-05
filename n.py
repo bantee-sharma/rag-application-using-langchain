fddfbae6e1df77709ce4bb7386e78be27fd7bf42
bantee-sharma <banteesharma9@gmail.com>
2025-07-05 21:58:55 +0530

    Update n.py

--- n.py
+++ n.py
@@ -5,7 +5,20 @@ class Car():
 
 
 c = Car("BMW","Black")
-print(c.brand_name)
+# print(c.brand_name)
+# print(c.color_name)
 
-
+class Specs(Car):
+    def __init__(self, brand, color,gear, seat, dash_cam):
+        super().__init__(brand, color)
+        self.gear = gear
+        self.seat = seat
+        self.dash_cam = dash_cam
+    
+s = Specs("BMW","Black",6,5,"Yes")
+print(s.brand_name)
+print(s.dash_cam)
+print(s.color_name)
+print(s.gear)
+print(s.seat)
         
