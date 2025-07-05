7c15b285add98e8db35f63b2094ecd646a192ea0
bantee-sharma <banteesharma9@gmail.com>
2025-07-05 21:35:58 +0530

    Update n.py

--- n.py
+++ n.py
@@ -1,10 +1,20 @@
 
 
+# class Car:
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+#         return a*b
+    
+# c = Car(5,5)
+# print(c)
+
+
 class Car:
     def func(self,a,b):
-        a = self.a
-        b = self.b
+        self.a = a
+        self.b = b
         return a*b
     
 c = Car()
-print(c.func(3,5))
+print(c.func(5,5))
