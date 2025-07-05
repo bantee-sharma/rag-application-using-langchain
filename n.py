26c177895df250ea56a6b396e6154c64c21ec845
bantee-sharma <banteesharma9@gmail.com>
2025-07-05 21:47:34 +0530

    Update n.py

--- n.py
+++ n.py
@@ -1,31 +1,40 @@
 
 
-# class Student:
-#     def __init__(self,name,place):
-#         self.name = name
-#         self.place = place
-
-# s = Student("Bantee","Jewar")
-# print(s.name)
-# print(s.place)
-
-
-
 class Student:
-    def func(self,name,place):
+    def __init__(self,name,place):
         self.name = name
         self.place = place
-        return name,place
 
-s = Student()
-print(s.func("Bantee","jewar"))
+s = Student("Bantee","Jewar")
+print(s.name)
+print(s.place)
+
+class School(Student):
+    def __init__(self, scl_name):
+        self.scl_name = scl_name
+
+scl = School()
+
+print(scl.name)
 
 
 
-class Student:
-    @staticmethod
-    def func(name,place):
-        return name,place
 
-s = Student()
-print(s.func("Bantee","jewar"))
+# class Student:
+#     def func(self,name,place):
+#         self.name = name
+#         self.place = place
+#         return name,place
+
+# s = Student()
+# print(s.func("Bantee","jewar"))
+
+
+
+# class Student:
+#     @staticmethod
+#     def func(name,place):
+#         return name,place
+
+# s = Student()
+# print(s.func("Bantee","jewar"))
