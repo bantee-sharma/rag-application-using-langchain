e3c00e9123b36977b686e6e4ddba2b644e897954
bantee-sharma <banteesharma9@gmail.com>
2025-07-05 21:44:40 +0530

    Update n.py

--- n.py
+++ n.py
@@ -11,13 +11,14 @@
 
 
 
-# class Student:
-#     def func(self,name,place):
-#         self.name = name
-#         self.place = place
+class Student:
+    def func(self,name,place):
+        self.name = name
+        self.place = place
+        return name,place
 
-# s = Student()
-# print(s.func("Bantee","jewar"))
+s = Student()
+print(s.func("Bantee","jewar"))
 
 
 
