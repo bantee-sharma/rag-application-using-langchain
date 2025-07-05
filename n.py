eb2a0eaf5a933cd38c5053b0468c0070f0e5383b
bantee-sharma <banteesharma9@gmail.com>
2025-07-05 21:54:37 +0530

    Update n.py

--- n.py
+++ n.py
@@ -1,38 +1,11 @@
+class Car():
+    def __init__(self,brand,color):
+        self.brand_name = brand
+        self.color_name = color
 
 
-class Student:
-    def __init__(self,name,place):
-        self.name = name
-        self.place = place
-
-s = Student("Bantee","Jewar")
-
-class School(Student):
-    def __init__(self, scl_name):
-        self.scl_name = scl_name
-
-scl = School("SVM")
-print(scl.scl_name)
-print(s.name)
+c = Car("BMW","Black")
+print(c.brand_name)
 
 
-
-
-# class Student:
-#     def func(self,name,place):
-#         self.name = name
-#         self.place = place
-#         return name,place
-
-# s = Student()
-# print(s.func("Bantee","jewar"))
-
-
-
-# class Student:
-#     @staticmethod
-#     def func(name,place):
-#         return name,place
-
-# s = Student()
-# print(s.func("Bantee","jewar"))
+        
