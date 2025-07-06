b7a1dfd5bd39923c050deabccca497cfddb8c53a
bantee-sharma <banteesharma9@gmail.com>
2025-07-06 21:00:33 +0530

    Update n.py

--- n.py
+++ n.py
@@ -4,7 +4,7 @@ class Car():
         self.color_name = color
 
 
-c = Car("BMW","Black")
+# c = Car("BMW","Black")
 # print(c.brand_name)
 # print(c.color_name)
 
@@ -15,10 +15,20 @@ class Specs(Car):
         self.seat = seat
         self.dash_cam = dash_cam
     
-s = Specs("BMW","Black",6,5,"Yes")
-print(s.brand_name)
-print(s.dash_cam)
-print(s.color_name)
-print(s.gear)
-print(s.seat)
+# s = Specs("BMW","Black",6,5,"Yes")
+# print(s.brand_name)
+# print(s.dash_cam)
+# print(s.color_name)
+# print(s.gear)
+# print(s.seat)
+
+# ✅ This block will run only when you execute this file directly
+if __name__ == "__main__":
+    c = Car("BMW", "Black")
+    s = Specs("BMW", "Black", 6, 5, "Yes")
+    print(s.brand_name)
+    print(s.dash_cam)
+    print(s.color_name)
+    print(s.gear)
+    print(s.seat)
         
