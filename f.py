2f8ad5fd234727ef4cd5a69337c5ab632432d49a
bantee-sharma <banteesharma9@gmail.com>
2025-07-06 21:31:50 +0530

    Update f.py

--- f.py
+++ f.py
@@ -1,9 +1,2 @@
 import fitz
 
-file = fitz.open("docs/dl-curriculum.pdf")
-
-
-for page_no in range(len(file)):
-    page = file.load_page(page_no)
-    text = page.get_text()
-    print(f"Page no: {page_no} \n\ {"-"*30} \n\ {text}")
