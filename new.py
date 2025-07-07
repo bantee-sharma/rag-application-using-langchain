a6986c91f5829636b697928ee39118f2f92cf8e8
bantee-sharma <banteesharma9@gmail.com>
2025-07-07 21:09:34 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,6 +1,11 @@
-from n import Specs
+from langchain_google_genai import ChatGoogleGenerativeAI
+from youtube_transcript_api import YouTubeTranscriptApi,TranscriptsDisabled
 
-s = Specs()
 
-print(s)
+video_id = ""
 
+try:
+    transcript = YouTubeTranscriptApi.get_transcript(video_id=video_id, languages=["en","hi"])
+    print(transcript)
+except Exception as e:
+    print("No caption Available for this video")
