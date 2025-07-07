5315f057c3c72a4a5b57fbfb4ea4ffc1d9595499
bantee-sharma <banteesharma9@gmail.com>
2025-07-07 21:12:06 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,11 +1,19 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
-from youtube_transcript_api import YouTubeTranscriptApi,TranscriptsDisabled
+from youtube_transcript_api import YouTubeTranscriptApi,TranscriptsDisabled, NoTranscriptFound, VideoUnavailable
+
 
 
 video_id = ""
 
 try:
-    transcript = YouTubeTranscriptApi.get_transcript(video_id=video_id, languages=["en","hi"])
+    transcript = YouTubeTranscriptApi.get_transcript(video_id=video_id, languages=["en"])
     print(transcript)
+
+except TranscriptsDisabled:
+    print("❌ Transcripts are disabled for this video.")
+except NoTranscriptFound:
+    print("❌ No English transcript found for this video.")
+except VideoUnavailable:
+    print("❌ The video is unavailable or private.")
 except Exception as e:
-    print("No caption Available for this video")
+    print(f"❌ Unexpected error: {e}")
