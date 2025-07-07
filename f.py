22f91e633ae9199498f4ee487c54f17e8a6f2f79
bantee-sharma <banteesharma9@gmail.com>
2025-07-07 21:00:41 +0530

    Update f.py

--- f.py
+++ f.py
@@ -1,9 +1,11 @@
 import fitz
 
-docs = fitz.open("docs\dl-curriculum.pdf")
+docs = fitz.open("docs\PA - Consolidated lecture notes.pdf")
 
 for page_no in range(len(docs)):
     page = docs.load_page(page_no)
     text = page.get_text()
 
-    print(f"page_no.-{page_no} \n {"-"*30} \n {text}")
+    # print(f"page_no.-{page_no} \n {"-"*30} \n {text}")
+    image_list = page.get_images(full=True)
+    print(image_list)
