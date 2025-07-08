ab929d5769a3450db0b2808e0d92dd2b09cd07a7
bantee-sharma <banteesharma9@gmail.com>
2025-07-08 21:40:57 +0530

    Update screen.py

--- screen.py
+++ screen.py
@@ -3,9 +3,9 @@ def is_prime(n):
     if n<1:
         return False
     else:
-        for i in range(len(2,int(n**.05)+1)):
+        for i in range(2,int(n**0.5)+1):
             if n%i == 0:
-                return True
-        else:
-            return True
+                return False
+
+    return True
 print(is_prime(n))
