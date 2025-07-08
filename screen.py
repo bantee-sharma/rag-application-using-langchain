fad72c8bb3a82f69104b4901e3df5d775d09ee05
bantee-sharma <banteesharma9@gmail.com>
2025-07-08 21:42:57 +0530

    Update screen.py

--- screen.py
+++ screen.py
@@ -1,4 +1,4 @@
-n = int(input("enter your input"))
+# n = int(input("enter your input: "))
 def is_prime(n):
     if n<1:
         return False
@@ -6,6 +6,8 @@ def is_prime(n):
         for i in range(2,int(n**0.5)+1):
             if n%i == 0:
                 return False
-
     return True
-print(is_prime(n))
+
+lst = [1,2,3,4,5,6,7,8,9,10,11]
+def prime(lst):
+    return [i for i in lst if is_prime(i)]
