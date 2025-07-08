abc4f2a3dd62e152b81fcede4411022a902e055b
bantee-sharma <banteesharma9@gmail.com>
2025-07-08 21:39:03 +0530

    Update screen.py

--- screen.py
+++ screen.py
@@ -1,3 +1,11 @@
-def func(a,b):
-    return a/b
-print(int(func(10,5)))
+n = int(input("enter your input"))
+def is_prime(n):
+    if n<1:
+        return False
+    else:
+        for i in range(len(2,int(n**.05)+1)):
+            if n%i == 0:
+                return True
+        else:
+            return True
+print(is_prime(n))
