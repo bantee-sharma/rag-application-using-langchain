4cd2d466498062c0588fc25f28aa822923fbaa8c
bantee-sharma <banteesharma9@gmail.com>
2025-07-09 22:06:32 +0530

    del

--- remove-bg.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-from rembg import remove
-
-input_path = "IMG_20250329_1432297.jpg"
-output_img = ""
-
-input = Image.open(input_path)
-output = remove(input)
-output.save(output_img)
